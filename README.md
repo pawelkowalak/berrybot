913ddc050f31a4ae4db02053b4ab4afb6ae01787
Paweł Kowalak <pawel.kowalak@gmail.com>
2015-10-13 11:15:34 +0200

    Send normalized input via GRPC to server
    Renamed berry_client to build installable .apk. Changed GRPC to stream with
    X and Y values (based on stick controller in the mobile app). Added sample
    assets for controller.

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 Generate proto:
 
 `protoc -I ./proto/ ./proto/steering.proto --go_out=plugins=grpc:proto`
+
+Install and run mobile app locally:
+
+`go install github.com/viru/berrybot/berrycli && berrycli`
+
+Install mobile app on connected device:
+
+`gomobile install github.com/viru/berrybot/berrycli`
+
+Install and run server:
+
+`go install github.com/viru/berrybot/berry_server && berry_server`
--- berry_server/main.go
+++ berry_server/main.go
@@ -2,27 +2,40 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net"
 
-	"golang.org/x/net/context"
 	"google.golang.org/grpc"
-	"google.golang.org/grpc/grpclog"
 
+	log "github.com/Sirupsen/logrus"
 	pb "github.com/viru/berrybot/proto"
 )
 
 // server is used to implement hellowrld.GreeterServer.
 type server struct{}
 
-func (s *server) Forward(ctx context.Context, in *pb.ForwardRequest) (*pb.ForwardReply, error) {
-	grpclog.Printf("got message! %t", in.Ok)
-	return &pb.ForwardReply{Ok: true}, nil
+func (s *server) Drive(stream pb.Driver_DriveServer) error {
+	for {
+		direction, err := stream.Recv()
+		if err == io.EOF {
+			return stream.SendAndClose(&pb.DirectionReply{
+				Ok: true,
+			})
+		}
+		if err != nil {
+			return err
+		}
+		log.WithFields(log.Fields{
+			"dx": direction.Dx,
+			"dy": direction.Dy,
+		}).Info("Direction")
+	}
 }
 
 func main() {
 	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 31337))
 	if err != nil {
-		grpclog.Fatalf("failed to listen: %v", err)
+		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
 	pb.RegisterDriverServer(s, &server{})
