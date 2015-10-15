f41c776c1e84027b7fea0b83450996a2de20d91a
Paweł Kowalak <pawel.kowalak@gmail.com>
2015-10-15 13:08:43 +0200

    Turn on/off motors based on provided gRPC inputs
    For now only forward and backward driving is possible. You need
    RaspberryPi2 + PiMotor DC hat module.

--- berry_server/main.go
+++ berry_server/main.go
@@ -17,9 +17,38 @@ type server struct{}
 
 var grpcPort = flag.String("grpc-port", "31337", "gRPC listen port")
 
+type gpioMock struct {
+	name string
+}
+
+func (g *gpioMock) High() {
+	// log.Infof("engine %s: high", g.name)
+}
+
+func (g *gpioMock) Low() {
+	// log.Infof("engine %s: low", g.name)
+}
+
 func (s *server) Drive(stream pb.Driver_DriveServer) error {
+	// err := rpio.Open()
+	// if err != nil {
+	// 	log.Fatalf("can't open rpio: %v", err)
+	// }
+	// defer rpio.Close()
+	// leftOn := rpio.Pin(23)
+	// leftFwd := rpio.Pin(4)
+	// rightOn := rpio.Pin(24)
+	// rightFwd := rpio.Pin(17)
+	// leftOn.Output()
+	// leftFwd.Output()
+	// rightOn.Output()
+	// rightFwd.Output()
+	leftOn := gpioMock{"leftOn"}
+	rightOn := gpioMock{"rightOn"}
+	leftFwd := gpioMock{"leftFwd"}
+	rightFwd := gpioMock{"rightFwd"}
 	for {
-		direction, err := stream.Recv()
+		d, err := stream.Recv()
 		if err == io.EOF {
 			return stream.SendAndClose(&pb.DirectionReply{
 				Ok: true,
@@ -28,10 +57,73 @@ func (s *server) Drive(stream pb.Driver_DriveServer) error {
 		if err != nil {
 			return err
 		}
-		log.WithFields(log.Fields{
-			"dx": direction.Dx,
-			"dy": direction.Dy,
-		}).Info("Direction")
+		// log.WithFields(log.Fields{
+		// 	"dx": d.Dx,
+		// 	"dy": d.Dy,
+		// }).Info("Direction")
+		switch {
+		// Full stop.
+		case d.Dy > -5 && d.Dy < 5 && d.Dx > -5 && d.Dx < 5:
+			leftOn.Low()
+			rightOn.Low()
+			log.Info("full stop")
+		// Forward.
+		case d.Dy > 5 && d.Dx > -5 && d.Dx < 5:
+			leftOn.High()
+			leftFwd.High()
+			rightOn.High()
+			rightFwd.High()
+			log.Info("fwd")
+		// Backward.
+		case d.Dy < -5 && d.Dx > -5 && d.Dx < 5:
+			leftOn.High()
+			leftFwd.Low()
+			rightOn.High()
+			rightFwd.Low()
+			log.Info("back")
+		// Sharp right.
+		case d.Dx > 5 && d.Dy > -5 && d.Dy < 5:
+			leftOn.High()
+			leftFwd.High()
+			rightOn.High()
+			rightFwd.Low()
+			log.Info("sharp right")
+		// Sharp left.
+		case d.Dx < -5 && d.Dy > -5 && d.Dy < 5:
+			leftOn.High()
+			leftFwd.High()
+			rightOn.High()
+			rightFwd.Low()
+			log.Info("sharp left")
+		// Forward + right.
+		case d.Dx > 5 && d.Dy > 5:
+			leftOn.High()
+			leftFwd.High()
+			rightOn.Low()
+			rightFwd.High()
+			log.Info("forward right")
+		// Forward + left.
+		case d.Dx < -5 && d.Dy > 5:
+			leftOn.Low()
+			leftFwd.High()
+			rightOn.High()
+			rightFwd.High()
+			log.Info("forward left")
+		// Backward + right.
+		case d.Dx > 5 && d.Dy < -5:
+			leftOn.High()
+			leftFwd.Low()
+			rightOn.Low()
+			rightFwd.Low()
+			log.Info("back right")
+		// Backward + left.
+		case d.Dx < -5 && d.Dy < -5:
+			leftOn.Low()
+			leftFwd.Low()
+			rightOn.High()
+			rightFwd.Low()
+			log.Info("back left")
+		}
 	}
 }
 
